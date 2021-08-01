31649dc24b25eb9a81b6d02126c6f348d5113a18
blackeagle94 <blackeagle4894@gmail.com>
2021-08-01 13:00:56 +0200

    google login fixed

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,83 +1,87 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import {Link, Redirect} from 'react-router-dom'
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import './Login.css';
 import GoogleLogin from 'react-google-login';
 import googleLogo from './grommet-icons_google.jpg';
+require('dotenv');
 
-const Login = ({setIsLoggedIn}) => {
-	const [users, setUsers] = useState([])
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('') 
-	const [userExist, setUserExist] = useState(false)
+const Login = ({ setIsLoggedIn }) => {
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ userExist, setUserExist ] = useState(false);
 
-	useEffect( 
-		() => {
-			window.scroll(0, 0)
-			axios
-				.get('https://mern-brothers.herokuapp.com/users')
-				.then((res) => {
-					setUsers(res.data);
-				})
-				.catch((err) => console.log(err));
-		},
-		[]
-	);
+	const history = useHistory();
 
 	const responseGoogle = (response) => {
-		if(!response.googleId) return document.querySelector('.valid').textContent = 'Something went wrong try again or sign in manually'
-		const { email} = response.profileObj
-		if(users) {
-			for(let i = 0; i < users.length; i++) {
-				if(users[i].email === email) {
-					setUserExist(true)
-					setIsLoggedIn(true)
-					return
-				}
-			}
-			document.querySelector('.valid').textContent = 'This account does not exist. Please sign up first.'
-		}
+		if (!response.googleId)
+			return (document.querySelector('.valid').textContent =
+				'Something went wrong try again or sign in manually');
+		const { email, googleId } = response.profileObj;
+		axios
+			.post('http://localhost:5001/users/signin', {
+				email: email,
+				password: googleId
+			})
+			.then((res) => {
+				sessionStorage.setItem('userInfo', JSON.stringify(res.data));
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		window.scroll(0, 0);
+		document.querySelector('.valid').textContent = 'Signed up successfully. Redirecting to homepage...';
+		setTimeout(() => {
+			setIsLoggedIn(true);
+			history.push('/');
+		}, 1500);
 	};
 
 	const login = (e) => {
-	
-		if(email.trim() === '' || password.trim() === '') {
-			document.querySelector('.valid').textContent = 'Please fill the blanks!'
-			return false
+		if (email.trim() === '' || password.trim() === '') {
+			document.querySelector('.valid').textContent = 'Please fill the blanks!';
+			return false;
 		}
-		const username = email.includes('@') ? {
-			email: email,
-			password:password
-		} : {
-			username: email,
-			password:password
-		}
-		axios.post('https://mern-brothers.herokuapp.com/signin', username)
-		.then(res => {
-			setUserExist(true)
-			setIsLoggedIn(true)
-			sessionStorage.setItem('userInfo', JSON.stringify(res.data))
-		
-			console.log(res.data)
-		})
-		.catch(err => {
-			setUserExist(false)
-			document.querySelector('.valid').textContent = 'Invalid email or password!'
-			console.log(err)
-		})
-	}
+		const username = email.includes('@')
+			? {
+					email: email,
+					password: password
+				}
+			: {
+					username: email,
+					password: password
+				};
+		axios
+			.post('http://localhost:5001/users/signin', username)
+			.then((res) => {
+				setUserExist(true);
+				setIsLoggedIn(true);
+				sessionStorage.setItem('userInfo', JSON.stringify(res.data));
 
-	if(userExist) {
-		return <Redirect to='/'/>
+				console.log(res.data);
+			})
+			.catch((err) => {
+				setUserExist(false);
+				document.querySelector('.valid').textContent = 'Invalid email or password!';
+				console.log(err);
+			});
+	};
+
+	if (userExist) {
+		return <Redirect to="/" />;
 	}
 
 	return (
 		<div className="sign-in">
 			<div className="sign-in-body">
 				<h1 className="sign-in-SignIn">Sign in</h1>
-				<h3 className="valid" style={{color:'red'}}> </h3>
-				<form className="sign-in-form" >
-				<label style={{color:"#347ca5"}} htmlFor="email">Email or Username</label>
+				<h3 className="valid" style={{ color: 'red' }}>
+					{' '}
+				</h3>
+				<form className="sign-in-form">
+					<label style={{ color: '#347ca5' }} htmlFor="email">
+						Email or Username
+					</label>
 					<input
 						className="sign-in-email"
 						type="text"
@@ -88,7 +92,9 @@ const Login = ({setIsLoggedIn}) => {
 						onChange={(e) => setEmail(e.target.value)}
 						required
 					/>
-					<label style={{color:"#347ca5"}} htmlFor="password">Password</label>
+					<label style={{ color: '#347ca5' }} htmlFor="password">
+						Password
+					</label>
 					<input
 						className="sign-in-password"
 						type="password"
@@ -99,7 +105,9 @@ const Login = ({setIsLoggedIn}) => {
 						onChange={(e) => setPassword(e.target.value)}
 						required
 					/>
-					<Link onClick={ login } className="link" to={userExist ? '/' : '/login'}>Sign in</Link>
+					<Link onClick={login} className="link" to={userExist ? '/' : '/login'}>
+						Sign in
+					</Link>
 					<GoogleLogin
 						render={(renderProps) => (
 							<button
@@ -112,7 +120,7 @@ const Login = ({setIsLoggedIn}) => {
 							</button>
 						)}
 						className="sign-up-google"
-						clientId="906847324262-e38d6kiavhpnl6nu22g4u03ms5h4ul42.apps.googleusercontent.com"
+						clientId="906847324262-l8eosmhanc5hq8015f5sq2dle6r6hh62.apps.googleusercontent.com"
 						buttonText="Join with Google"
 						onSuccess={responseGoogle}
 						onFailure={responseGoogle}
