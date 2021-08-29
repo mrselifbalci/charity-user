58faa8691681ee1669420eaa1f5c11803729b8ce
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-08-29 06:31:08 +0100

    updated donate goods

--- src/components/GetInvolved/DonateGoods/DonateGoodsForm.js
+++ src/components/GetInvolved/DonateGoods/DonateGoodsForm.js
@@ -7,17 +7,14 @@ import axios from 'axios';
 
 const DonateGoodsForm = () => {
 	const [modalIsOpen, setModalIsOpen] = useState(false);
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
 	const [phone, setPhone] = useState('');
-	const [email, setEmail] = useState('');
 	const [typeOfGoods, setTypeOfGoods] = useState('');
 	const [numberOfPieces, setNumberOfPieces] = useState('');
 	const [address, setAddress] = useState('');
-	const [postCode, setPostCode] = useState('');
+	const [postcode, setPostcode] = useState('');
 	const [instructions, setInstructions] = useState('');
 	const [userId, setUserId] = useState(
-		JSON.parse(sessionStorage.getItem('userInfo')).id,
+		JSON.parse(sessionStorage.getItem('userInfo')).id
 	);
 
 	useLayoutEffect(() => {
@@ -33,104 +30,69 @@ const DonateGoodsForm = () => {
 		e.preventDefault();
 		axios
 			.post('https://charity-backend-july.herokuapp.com/donations', {
-				firstname: firstName,
-				lastname: lastName,
-				email: email,
-				phone: phone,
+				phone,
 				type_of_goods: typeOfGoods,
 				number_of_pieces: numberOfPieces,
 				address: address,
-				post_code: postCode,
-				instructions_for_the_driver: instructions,
-				user_id: userId,
-				type:"donate-good-form"
+				postcode,
+				instructions,
+				userId,
+				type: 'good',
 			})
 			.then((res) => console.log(res))
 			.catch((err) => console.log(err));
 
-		setFirstName('');
-		setLastName('');
 		setPhone('');
-		setEmail('');
 		setTypeOfGoods('');
 		setNumberOfPieces('');
 		setAddress('');
-		setPostCode('');
+		setPostcode('');
 		setInstructions('');
 		setModalIsOpen(true);
 	};
 
 	return (
 		<div>
-			<div className="donate-goods-header-container">
+			<div className='donate-goods-header-container'>
 				<img
 					src={imageHeader}
-					alt="Avatar"
-					className="donate-goods-header-image"
+					alt='Avatar'
+					className='donate-goods-header-image'
 				/>
-				<div className="donate-goods-header-overlay">
-					<h1 className="donate-goods-header-text">Donate Goods</h1>
+				<div className='donate-goods-header-overlay'>
+					<h1 className='donate-goods-header-text'>Donate Goods</h1>
 				</div>
 			</div>
-			<div className="donate-goods-form-container">
+			<div className='donate-goods-form-container'>
 				<form onSubmit={submitForm}>
-					<div className="donate-goods-form">
-						<div className="donate-goods-form-left">
-							<div>
-								<label>First Name</label>
-								<input
-									type="text"
-									placeholder="Enter your first name"
-									value={firstName}
-									onChange={(e) => setFirstName(e.target.value)}
-									required
-								/>
-							</div>
-							<div>
-								<label>Last Name</label>
-								<input
-									type="text"
-									placeholder="Enter your last name"
-									value={lastName}
-									onChange={(e) => setLastName(e.target.value)}
-									required
-								/>
-							</div>
-							<div className="donate-goods-form-contact-no">
+					<div className='donate-goods-form'>
+						<div className='donate-goods-form-left'>
+							<div className='donate-goods-form-contact-no'>
 								<label>
 									Contact Number{' '}
-									<span className="donate-goods-form-contact-no-format">
+									<span className='donate-goods-form-contact-no-format'>
 										<i>(Format XXX-XXXX-XXXX)</i>
 									</span>
 								</label>
 								<input
-									type="tel"
-									className="donate-goods-form-contact-no-input"
-									pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
-									placeholder="Enter contact number"
+									type='tel'
+									className='donate-goods-form-contact-no-input'
+									pattern='[0-9]{3}-[0-9]{4}-[0-9]{4}'
+									placeholder='Enter contact number'
 									value={phone}
 									onChange={(e) => setPhone(e.target.value)}
 									required
 								/>
 							</div>
-							<div>
-								<label>Email Address</label>
-								<input
-									type="email"
-									placeholder="Enter your email address"
-									value={email}
-									onChange={(e) => setEmail(e.target.value)}
-									required
-								/>
-							</div>
+
 							<div>
 								<label>
 									Type of goods
 									<br /> (books,clothes etc.)
 								</label>
 								<input
-									type="text"
-									placeholder="Enter type of goods"
+									type='text'
+									placeholder='Enter type of goods'
 									value={typeOfGoods}
 									onChange={(e) => setTypeOfGoods(e.target.value)}
 									required
@@ -139,20 +101,20 @@ const DonateGoodsForm = () => {
 							<div>
 								<label>Number of Pieces</label>
 								<input
-									type="number"
-									placeholder="Enter number of pieces"
+									type='number'
+									placeholder='Enter number of pieces'
 									value={numberOfPieces}
 									onChange={(e) => setNumberOfPieces(e.target.value)}
 									required
 								/>
 							</div>
 						</div>
-						<div className="donate-goods-form-right">
+						<div className='donate-goods-form-right'>
 							<div>
 								<label>Address</label>
 								<textarea
-									type="text"
-									placeholder="Enter the full address"
+									type='text'
+									placeholder='Enter the full address'
 									value={address}
 									onChange={(e) => setAddress(e.target.value)}
 									required
@@ -161,18 +123,18 @@ const DonateGoodsForm = () => {
 							<div>
 								<label>Post Code</label>
 								<input
-									type="text"
-									placeholder="Enter the post code"
-									value={postCode}
-									onChange={(e) => setPostCode(e.target.value)}
+									type='text'
+									placeholder='Enter the post code'
+									value={postcode}
+									onChange={(e) => setPostcode(e.target.value)}
 									required
 								/>
 							</div>
 							<div>
 								<label>Instructions for the Driver</label>
 								<textarea
-									type="text"
-									placeholder="Enter a clear instruction for the driver "
+									type='text'
+									placeholder='Enter a clear instruction for the driver '
 									value={instructions}
 									onChange={(e) => setInstructions(e.target.value)}
 									required
@@ -180,12 +142,11 @@ const DonateGoodsForm = () => {
 							</div>
 						</div>
 					</div>
-					<div className="donate-goods-form-submit-container">
+					<div className='donate-goods-form-submit-container'>
 						<button
-							type="submit"
-							className="donate-goods-form-submit-btn"
-							onClick={() => window.scroll(0, 0)}
-						>
+							type='submit'
+							className='donate-goods-form-submit-btn'
+							onClick={() => window.scroll(0, 0)}>
 							Submit
 						</button>
 					</div>
@@ -195,27 +156,25 @@ const DonateGoodsForm = () => {
 			<Modal
 				isOpen={modalIsOpen}
 				onRequestClose={() => setModalIsOpen(false)}
-				className="donate-goods-form-modal"
-			>
-				<span className="donate-goods-form-modal-text">
+				className='donate-goods-form-modal'>
+				<span className='donate-goods-form-modal-text'>
 					Thank you! <br />
-					Your donation is currently under review. Once approved, you will
-					be contacted via email regarding the collection time in your area
+					Your donation is currently under review. Once approved, you will be
+					contacted via email regarding the collection time in your area
 				</span>
 				<br />
 				<br />
 				<button
-					to="/getinvolved/donategoods-form"
-					className="donate-goods-form-btn-popup"
+					to='/getinvolved/donategoods-form'
+					className='donate-goods-form-btn-popup'
 					onClick={() => {
 						makeAnotherDonation();
 						window.scroll(0, 0);
-					}}
-				>
+					}}>
 					Make Another Donation
 				</button>
 				<br />
-				<Link to="/" className="donate-goods-form-btn-popup">
+				<Link to='/' className='donate-goods-form-btn-popup'>
 					Back to Home Page
 				</Link>
 			</Modal>
